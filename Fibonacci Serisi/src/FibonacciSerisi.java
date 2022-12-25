3194f84fe4e060af4e5678e3760788d8fa5917e4
Mert sezgin <sezgincanmert@gmail.com>
2022-12-25 16:12:11 +0300

    Update FibonacciSerisi.java

--- Fibonacci Serisi/src/FibonacciSerisi.java
+++ Fibonacci Serisi/src/FibonacciSerisi.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 public class FibonacciSerisi {
 
 	public static void main(String[] args) {
-		
-		
+		    int top=0,a,b=1;
+		    
+		      Scanner scan = new Scanner(System.in);
+		    
+		    System.out.println("sayýyý giriniz");
+		    int sayi = scan.nextInt();
 
+		    for(int i=0; i<sayi;i++){
+		        
+		      a=b;
+		      b=top;
+		      top=a+b;
+		      System.out.println(top);
+		    }
+		  }
 	}
 
-}
